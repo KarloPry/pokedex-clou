f51c7f54fd397a944db049f2e762ed412d96eabe
KarloPry <kpaz30@alumnos.uaq.mx>
2024-05-22 08:37:09 -0600

    fixed env variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
+    UPLOADTHING_APP_ID: process.env.UPLOADTHING_APP_ID,
+  },
   images: {
     domains: ["assets.pokemon.com", "utfs.io"],
   },
